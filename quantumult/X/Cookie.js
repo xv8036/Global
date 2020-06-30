27677398da09179f4e96ac80528d144b25b3bedf
xv8036 <54239611+xv8036@users.noreply.github.com>
2020-06-30 17:46:41 +0800

    Delete Cookie.js

--- quantumult/X/Cookie.js
+++
@@ -1,7 +0,0 @@
-hostname = passport.suning.com, luckman.suning.com, sign.suning.com, gameapi.suning.com
-
-# 苏宁获取Cookie
-^https:\/\/passport.suning.com\/ids\/login$ url script-request-body https://github.com/xv8036/Global/raw/master/Javascripts/suning.cookie.js
-^https:\/\/luckman.suning.com\/luck-web\/sign\/api\/clock_sign.do url script-request-header https://github.com/xv8036/Global/raw/master/Javascripts/suning.cookie.js
-^https:\/\/sign.suning.com\/sign-web\/m\/promotion\/sign\/doSign.do url script-request-header https://github.com/xv8036/Global/raw/master/Javascripts/suning.cookie.js
-^https:\/\/gameapi.suning.com\/sngame-web\/(api\/signin\/private\/customerSignOperation.do|gateway\/api\/queryPrize.do) url script-request-header https://github.com/xv8036/Global/raw/master/Javascripts/suning.cookie.js

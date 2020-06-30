816f506670c650e88becf435ba67479ae105351c
xv8036 <54239611+xv8036@users.noreply.github.com>
2020-06-30 17:06:54 +0800

    Update and rename Cookies.js to Cookie.js

---
+++ quantumult/X/Cookie.js
@@ -0,0 +1,5 @@
+# 苏宁获取Cookie
+^https:\/\/passport.suning.com\/ids\/login$ url script-request-body suning.cookie.js
+^https:\/\/luckman.suning.com\/luck-web\/sign\/api\/clock_sign.do url script-request-header suning.cookie.js
+^https:\/\/sign.suning.com\/sign-web\/m\/promotion\/sign\/doSign.do url script-request-header suning.cookie.js
+^https:\/\/gameapi.suning.com\/sngame-web\/(api\/signin\/private\/customerSignOperation.do|gateway\/api\/queryPrize.do) url script-request-header suning.cookie.js
